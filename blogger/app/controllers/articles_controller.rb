cc455660be8c17090cdeb97bd6785dcde5bee2e0
Cody Buffaloe <codylbuffaloe@gmail.com>
2016-03-17 12:22:49 -0400

    Added partial and streamlined view pages edit and new. finished edit and update methods in controller

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -19,6 +19,17 @@ class ArticlesController < ApplicationController
         redirect_to articles_path(@articles)
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        flash.notice = "Article '#{@article.title}' Updated!"
+        redirect_to article_path(@article)
+    end
+
     def destroy
         @article = Article.find(params[:id])
         @article.destroy
